c18bd28b05066d3f8ce6ac80d0922053401bf93b
EC2 Default User <ec2-user@ip-172-31-36-21.us-west-2.compute.internal>
2025-12-30 19:43:04 +0000

    Fix typo in stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Add this at the top of your script to make it exit immediately if any command fails
-set -e
+isExistApp="$(pgrep httpd)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop httpd.service
+fi
+isExistApp="$(pgrep tomcat)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop tomcat.service
+fi
 
-# Your intentional error
-sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
-
-# To be extra sure, we can also add an explicit exit code
-exit 1
